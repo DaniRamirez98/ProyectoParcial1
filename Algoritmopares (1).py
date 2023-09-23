2b6469b80a53c43b9879afe26746ddc34fbf0d14
DaniRamirez98 <145822848+DaniRamirez98@users.noreply.github.com>
2023-09-22 23:51:55 -0600

    Add files via upload

---
+++ Algoritmopares (1).py
@@ -0,0 +1,10 @@
+N=input("Ingresa un numero: ")
+N=int(N)
+#lista del 1 a N
+lista=list(range(1,N+1))
+#multiplicar los numeros de la lista por 2
+lista2=[i*2 for i in lista]
+#suma de los numeros de la lista
+suma=sum(lista2)
+#imprimir la suma
+print(suma)
